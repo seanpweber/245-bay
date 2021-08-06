e58f4b603204e5d640ba37fafc59bd4a3e0e3406
Sean Weber <sean.weber95@gmail.com>
2021-08-06 09:50:06 -0400

    cleanups and repairs

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ console.log("server started");
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-// const routes = require('./app/routes/api/project.routes');
-// const sequelize = require('./app/config/connection');
 
 const app = express();
 var PORT = process.env.PORT || 4000;
@@ -14,13 +12,7 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(cors());
-
-// app.get('/projects/:id', function (req, res, next) {
-//   res.json({msg: 'This is CORS-enabled for all origins!'})
-// })
-
-// app.use('/api/projects', routes);
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to zzzleepy's server." });
@@ -35,10 +27,6 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(express.static(__dirname + "/client"));
 }
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
 
 serv.listen(PORT, function() {
     console.log('Server is running on http://localhost:' + PORT);
